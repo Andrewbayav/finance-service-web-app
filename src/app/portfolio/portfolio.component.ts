11d2d6ec3c3f5396407e4f78e5eb1e538b139f8a
abaykin <abaykin@fork.ru>
2022-01-25 14:49:44 +0300

    Рефакторинг

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgModule, OnInit} from '@angular/core';
 import {ManagementHttpService} from "../services/management-http.service";
 import {environment} from "../../environments/environment";
+import {RingChartComponent} from "../charts/ring-chart/ring-chart.component";
+import {sum} from '@taiga-ui/cdk';
+
 
 @Component({
   selector: 'app-portfolio',
@@ -15,20 +18,30 @@ export class PortfolioComponent implements OnInit {
   token: string = '';
 
   ngOnInit(): void {
-      this.token = environment.token;
-      this.getPortfolio();
+    this.token = environment.token;
+    this.getPortfolio();
   }
+  filtersLoaded: Promise<boolean>;
 
   chartData:any = [];
   portfolioData: any = [];
   loaded: boolean = false;
 
+  // table -----------------------------
   ELEMENT_DATA:any = [];
   PORTFOLIO_TABLE:any = [];
-  displayedColumns: string[] = ['position', 'name', 'ticker', 'purchasePrice', 'priceUsd', 'sumUSD'];
+  displayedColumns: string[] = [ 'name', 'ticker', 'purchasePrice', 'priceUsd', 'sumUSD',
+  'cap', 'enterpriseValue', 'priceToBook', 'priceToSalesTrailing12Months', 'trailingPE',
+  'forwardPE', 'enterpriseToEbitda', 'dividendYield', 'recommendationMean'];
+  // table -----------------------------
+
+  value: number[] = [];
+  labels: string[] = [];
+  sum: number;
 
   getPortfolio() {
     this.managementService.requestTinkoffPortfolio(this.token).subscribe(res => {
+      debugger
       let i = 1;
       // @ts-ignore
       res.forEach(el => {
@@ -36,15 +49,49 @@ export class PortfolioComponent implements OnInit {
         let tmp = {name: el.name, value: el.sumUSD}
         this.portfolioData.push(tmp);
         // for table
-        let tableTmp = {position: i, name: el.name, ticker: el.ticker, purchasePrice: el.purchasePrice.toFixed(2), priceUsd: el.priceNowUSD.toFixed(2), sumUSD: el.sumUSD.toFixed(2)}
-        debugger
+        let tableTmp = {
+          position: i,
+          name: el.name,
+          ticker: el.ticker,
+          purchasePrice: el.purchasePrice.toFixed(2),
+          priceUsd: el.priceUsd.toFixed(2),
+          sumUsd: el.sumUsd.toFixed(2),
+          cap: this.numberWithCommas(el.marketCap),
+          enterpriseValue: this.numberWithCommas(el.enterpriseValue),
+          priceToBook: el.priceToBook.toFixed(2),
+          priceToSalesTrailing12Months: el.priceToSalesTrailing12Months.toFixed(2),
+          trailingPE: el.trailingPE.toFixed(2),
+          forwardPE: el.forwardPE.toFixed(2),
+          enterpriseToEbitda: el.enterpriseToEbitda.toFixed(2),
+          dividendYield: el.dividendYield.toFixed(2),
+          recommendationMean: el.recommendationMean
+        }
         this.PORTFOLIO_TABLE.push(tableTmp);
+        this.value.push(el.sumRUB);
+        this.labels.push(el.name);
         i++;
       })
+      this.sum = this.getArrElementsSum(this.value);
       this.chartData = this.portfolioData;
       this.ELEMENT_DATA = this.PORTFOLIO_TABLE;
       this.loaded = true;
+      this.filtersLoaded = Promise.resolve(true); // Setting the Promise as resolved after I have the needed data
     });
+    debugger
   }
+
+  getArrElementsSum(a: number[]) {
+    let sum: number = 0;
+    a.forEach(el => {
+      sum += el;
+    })
+    return sum;
+  }
+
+  numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+
 
 }
